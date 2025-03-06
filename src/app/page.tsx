300ceddc0c875f15ae92084b9a8e2423fc9d68e6
vinicius-pascoal <112434231+vinicius-pascoal@users.noreply.github.com>
2025-03-06 15:38:27 -0300

    header start

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,20 @@
+import logo from "../img/logo.jpg";
+import Image from "next/image";
 
 export default function Home() {
   return (
-    <div>
-      
-    </div>
+    <header className="bg-orange-400 border-b-amber-500 ">
+      <div>
+
+      </div>
+      <Image
+        className="rounded-full"
+        src={logo}
+        alt="Next.js logo"
+        width={130}
+        height={130}
+        priority
+      />
+    </header>
   );
 }
