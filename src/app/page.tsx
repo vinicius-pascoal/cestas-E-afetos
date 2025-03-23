ff04b1f84fbdca92ff66834f70818c8eb41c436b
vinicius-pascoal <112434231+vinicius-pascoal@users.noreply.github.com>
2025-03-23 18:28:47 -0300

    add hex cestas

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,20 +1,34 @@
 import logo from "../img/logo.jpg";
+import cafeManhaCasal from "../img/cestas/cafeManhaCasal.jpg";
 import Image from "next/image";
 
 export default function Home() {
   return (
-    <header className="bg-orange-400 border-b-amber-500 ">
-      <div>
-
-      </div>
-      <Image
-        className="rounded-full"
-        src={logo}
-        alt="Next.js logo"
-        width={130}
-        height={130}
-        priority
-      />
-    </header>
+    <div className="w-dvw">
+      <header className="bg-orange-200 border-b-4 border-b-amber-500 ">
+        <div></div>
+        <Image
+          className="rounded-full"
+          src={logo}
+          alt="Next.js logo"
+          width={130}
+          height={130}
+          priority
+        />
+      </header>
+      <ul id="hexGrid">
+        <li className="hex">
+          <a className="hexIn" href="#">
+            <Image
+              src={cafeManhaCasal}
+              alt=""
+            />
+            <h1>Cafe da Manha de casal</h1>
+            <p>Some sample text about the article this hexagon leads to</p>
+          </a>
+        </li>
+        
+      </ul>
+    </div>
   );
 }
