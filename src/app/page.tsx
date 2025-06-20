4c51bf6b1af108a480ff250a0bd7622b5e18413a
vinicius-pascoal <112434231+vinicius-pascoal@users.noreply.github.com>
2025-06-20 09:00:18 -0300

    init item card

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
 import logo from "../img/logo.jpg";
+import cafeSolo from "../img/cestas/cafeManhaSolo.jpg";
 import Image from "next/image";
 import WallpaperContainer from "@/components/WallpaperContainer";
 
@@ -7,9 +8,9 @@ export default function Home() {
     <WallpaperContainer>
       <div className="w-dvw">
         {/* TODO melhorar header */}
-        <header className="mb-24 shadow-md shadow-amber-700 bg-[#dd7630] border-b-4 border-b-amber-700 h-14">
+        <header className="mb-36 bg-[#dd7630] border-b-4 border-b-amber-700 h-14">
           <Image
-            className=" shadow-md shadow-amber-700 rounded-full relative -bottom-5 ml-2 border-amber-700 border-4"
+            className=" rounded-full relative -bottom-5 ml-2 border-amber-700 border-4 w-28 h-28"
             src={logo}
             alt="Next.js logo"
             width={130}
@@ -19,13 +20,29 @@ export default function Home() {
         </header>
         <div>
           {/* TODO componente de cesta*/}
-          <h1 className="text-4xl text-center font-bold mt-8">
-            Bem-vindo ao Next.js!
-          </h1>
-          <p className="text-center mt-4">
-            Esta é uma aplicação de exemplo usando Next.js com animações e
-            imagens.
-          </p>
+          <div id="card" className=" m-7">
+            <div id="card-header" className=" shadow-md shadow-black flex bg-[#dd7630] text-white p-4 h-16">
+              <Image
+                src={cafeSolo}
+                alt="Logo"
+                width={100}
+                height={100}
+                className="relative -top-16 mr-2 rounded-full border-amber-700 border-4  w-24 h-24" />
+              <h1 className=" font-bold">Cesta de Café da Manhã</h1>
+            </div>
+            <div id="card-body" className=" bg-amber-700 text-white p-4 rounded-t-none rounded-lg shadow-md">
+              <p className="">
+                Desfrute de uma deliciosa cesta de café da manhã, perfeita para começar o dia com energia e sabor.
+              </p>
+              <ul className="list-disc pl-5 mt-2">
+                <li>Café fresco</li>
+                <li>Pão artesanal</li>
+                <li>Frutas da estação</li>
+                <li>Doces caseiros</li>
+              </ul>
+
+            </div>
+          </div>
         </div>
       </div>
     </WallpaperContainer>
